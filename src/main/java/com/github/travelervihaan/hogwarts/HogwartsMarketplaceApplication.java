4a0626515cef43cd60d3bf40e6ab5f8e9dd9391e
TravelerVihaan <lukasz.biel@10g.pl>
2019-11-05 00:02:59 +0100

    Initial commit

---
+++ src/main/java/com/github/travelervihaan/hogwarts/HogwartsMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.github.travelervihaan.hogwarts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartsMarketplaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HogwartsMarketplaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/travelervihaan/hogwarts/HogwartsMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.travelervihaan.hogwarts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsMarketplaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
