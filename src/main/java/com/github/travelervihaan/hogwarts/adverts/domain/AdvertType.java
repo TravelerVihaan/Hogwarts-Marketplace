b22b9242d57f5a8108c187743a70c6fccef2d828
TravelerVihaan <lukasz.biel@10g.pl>
2019-11-05 18:23:15 +0100

    First entities

---
+++ src/main/java/com/github/travelervihaan/hogwarts/adverts/domain/AdvertType.java
@@ -0,0 +1,7 @@
+package com.github.travelervihaan.hogwarts.adverts.domain;
+
+import javax.persistence.Entity;
+
+@Entity
+public class AdvertType {
+}
---
+++ src/main/java/com/github/travelervihaan/hogwarts/users/domain/UserRole.java
@@ -0,0 +1,7 @@
+package com.github.travelervihaan.hogwarts.users.domain;
+
+import javax.persistence.Entity;
+
+@Entity
+public class UserRole {
+}
