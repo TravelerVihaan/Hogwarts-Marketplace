e4d073a482ad4c869f8820b0c8b38b1dffeedfc6
TravelerVihaan <lukasz.biel@10g.pl>
2021-06-02 05:15:49 +0200

    Adv status entity

---
+++ src/main/java/com/github/travelervihaan/hogwarts/advertisements/domain/AdvertisementStatus.java
@@ -0,0 +1,5 @@
+package com.github.travelervihaan.hogwarts.advertisements.domain;
+
+public enum AdvertisementStatus {
+    WAITING, APPROVED, REJECTED, HIDDEN
+}
---
+++ src/main/java/com/github/travelervihaan/hogwarts/advertisements/entity/AdvertisementStatusEntity.java
@@ -0,0 +1,68 @@
+package com.github.travelervihaan.hogwarts.advertisements.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import java.util.Objects;
+
+@Entity
+@Table(name = "advertisement_statuses")
+public class AdvertisementStatusEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_advertisement_status")
+    private Long id;
+
+    @NotEmpty
+    @Column(name = "advertisement_status", nullable = false, unique = true)
+    private String advertisementStatus;
+
+    public AdvertisementStatusEntity() { }
+
+    public AdvertisementStatusEntity(Long id, String advertisementStatus) {
+        this.id = id;
+        this.advertisementStatus = advertisementStatus;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAdvertisementStatus() {
+        return advertisementStatus;
+    }
+
+    public void setAdvertisementStatus(String advertisementStatus) {
+        this.advertisementStatus = advertisementStatus;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AdvertisementStatusEntity that = (AdvertisementStatusEntity) o;
+        return Objects.equals(id, that.id) && Objects.equals(advertisementStatus, that.advertisementStatus);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, advertisementStatus);
+    }
+
+    @Override
+    public String toString() {
+        return "AdvertisementStatusEntity{" +
+                "id=" + id +
+                ", advertisementStatus='" + advertisementStatus + '\'' +
+                '}';
+    }
+}
